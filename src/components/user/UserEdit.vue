f64ed8387e12eb33a234f25978d42221f533f4c4
hegnemurd <hegne.murd@gmail.com>
2020-03-30 18:49:30 +0100

    Routing Project Vue Commit

---
+++ src/components/user/UserEdit.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h3>Edit the User</h3>
+    <p>Locale: {{ $route.query.locale }}</p>
+    <p>Analytics: {{ $route.query.q }}</p>
+    <hr />
+    <button class="btn btn-primary" @click="onClickConfirm">Confirm</button>
+    <div style="height: 700px"></div>
+    <p id="data">Some extra Data</p>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      confirmed: false
+    };
+  },
+  methods: {
+    onClickConfirm() {
+      this.$router.push({ name: "home" });
+    }
+  },
+  beforeRouteLeave(to, from, next) {
+    if (this.confirmed) {
+      next();
+    } else {
+      if (confirm("Are you sure?")) {
+        next();
+      } else {
+        next(false);
+      }
+    }
+  }
+};
+</script>
+<style scoped></style>
